ff1502b9e4110411684e1ab2939f67994b7d2ba8
LukeyDev <lukemadsen501@gmail.com>
2026-06-04 13:56:01 +1200

    Mirrored Procedural Textures
    Procedural texture generator that creates a random palette (with a 0,0,0,0 alpha color always) and then generates a 16x16 x-mirrored texture2D that can be/is applied to a sprite.

--- Assets/Scripts/SpriteGen/ProceduralTexture.cs
+++ Assets/Scripts/SpriteGen/ProceduralTexture.cs
@@ -7,7 +7,8 @@ public static class ProceduralTexture
     public static Color32[] CreatePalette(int numColors)
     {
         Color32[] palette = new Color32[numColors];
-        for (int i = 0; i < numColors; i++)
+        palette[0] = new Color32(0, 0, 0, 0);
+        for (int i = 1; i < numColors; i++)
         {
             Color32 color = new Color32((byte)Random.Range(0, 255), // r
                                         (byte)Random.Range(0, 255), // g
@@ -20,18 +21,21 @@ public static class ProceduralTexture
 
     public static Texture2D CreateTexture()
     {
-        Color32[] palette = CreatePalette(3);
+        Color32[] palette = CreatePalette(4);
         Debug.Log($"Color palette sample: {palette[0].r},{palette[0].g},{palette[0].b}   {palette[1].r},{palette[1].g},{palette[1].b}");
 
         Texture2D texture = new Texture2D(16, 16); // size x y
-        for (int x = 0; x < texture.width; x++)
+        for (int x = 0; x < texture.width/2; x++)
         {
             for (int y = 0; y < texture.height; y++)
             {
-                texture.SetPixel(x, y, palette[Random.Range(0, palette.Length)]);
+                Color32 color = palette[Random.Range(0, palette.Length)];
+                texture.SetPixel(x, y, color);
+                texture.SetPixel(texture.width-x-1, y, color);
             }
         }
 
+        texture.filterMode = FilterMode.Point;
         texture.Apply();
         return texture;
     }
