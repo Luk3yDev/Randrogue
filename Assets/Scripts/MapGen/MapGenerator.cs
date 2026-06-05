117e724b2e461604e8271b94410690d639202f21
LukeyDev <lukemadsen501@gmail.com>
2026-06-05 14:04:46 +1200

    Cellular Automata Testing
    Testing out using a cellular automata, but iteration size of 1,000,000 is far too large and won't load.

--- Assets/Scripts/MapGen/MapGenerator.cs
+++ Assets/Scripts/MapGen/MapGenerator.cs
@@ -5,11 +5,16 @@ using UnityEngine.Tilemaps;
 
 public class MapGenerator : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] Tilemap groundTilemap;
 
     public int SIZEX = 128;
     public int SIZEY = 256;
 
+    [Header("Cellular")]
+    public int iterations = 20;
+
+    [Header("Perlin")]
     public Vector2 amplitude;
     public Vector2 frequency;
     public Vector2 threshold;
@@ -37,15 +42,93 @@ public class MapGenerator : MonoBehaviour
         */
     }
 
+    int GetNeighbours(int[,] mapData, int x, int y, int me)
+    {
+        if (x == 0 ||
+            x == SIZEX-1 ||
+            y == 0 ||
+            y == SIZEY-1)
+        {
+            return 0;
+        }
+
+        int n = 0;
+        if (mapData[x+1,y  ] == me) { n++; }
+        if (mapData[x  ,y+1] == me) { n++; }
+        if (mapData[x+1,y+1] == me) { n++; }
+        if (mapData[x-1,y  ] == me) { n++; }
+        if (mapData[x  ,y-1] == me) { n++; }
+        if (mapData[x-1,y-1] == me) { n++; }
+        if (mapData[x+1,y-1] == me) { n++; }
+        if (mapData[x-1,y+1] == me) { n++; }
+        return n;
+    }
+
     public void GenerateMap()
     {
-        tilePalette = new Tile[Random.Range(1, 8)];
+        tilePalette = new Tile[Random.Range(2, 8)];
 
         for (int i = 0; i < tilePalette.Length; i++)
         {
             tilePalette[i] = ProceduralTile.CreateTile();
         }
 
+        //PerlinMethod();
+
+        int[,] mapData = new int[SIZEX,SIZEY];
+
+        int love = Random.Range(1,8); // cell rules
+        int hate = Random.Range(1,8);
+
+        for (int x = 0; x < SIZEX; x++) // 1. NOISE
+        {
+            for (int y = 0; y < SIZEY; y++)
+            {
+                mapData[x,y] = Random.Range(0, tilePalette.Length+1);
+            }
+        }
+        int[,] buffer = mapData; // create buffer to remove bias
+        for (int i = 0; i < iterations; i++) // 2. APPLY RULES
+        {
+            if (Random.Range(0, 5) == 0)
+            {
+                love = Random.Range(1,8);
+                hate = Random.Range(1,8); // reroll rules sometimes
+            }
+
+            for (int x = 0; x < SIZEX; x++)
+            {
+                for (int y = 0; y < SIZEY; y++)
+                {
+                    int me = mapData[x,y];
+                    int n = GetNeighbours(mapData, x, y, me);
+
+                    if (me != 0)
+                    {
+                        if (n > hate && n < love) buffer[x,y] = 0;
+                    }
+                    else
+                    {
+                        if (n < hate && n > love) buffer[x,y] = me;
+                    }
+                }
+            }
+        }
+        mapData = buffer; // swap buffers
+        for (int x = 0; x < SIZEX; x++) // 3. TILEMAP
+        {
+            for (int y = 0; y < SIZEY; y++)
+            {
+                Vector3Int tilepos = new Vector3Int(x, y, 0);
+
+                if (mapData[x,y] != 0)
+                    groundTilemap.SetTile(tilepos, tilePalette[mapData[x,y]-1]);
+            }
+        }
+    }
+
+    void PerlinMethod()
+    {
         int seed = Random.Range(-10000, 10000);
         Random.InitState(System.DateTime.Now.Millisecond);
 
@@ -66,7 +149,7 @@ public class MapGenerator : MonoBehaviour
 
                 float werm = wimble * glorb;
 
-                int tile = (int)(wimble * tilePalette.Length);
+                int tile = (int)((wimble + (2 * thresh)) * tilePalette.Length);
                     
                 if (wimble > thresh)
                 {
